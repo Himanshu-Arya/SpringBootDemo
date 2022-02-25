59e00b46e276f80570c462704d4ba44dc8fc7882
Himanshu-Arya <himanshu.arya48@gmail.com>
2022-02-25 23:19:09 +0530

    set db column name for primary_key field in department class

--- src/main/java/com/himanshu/SpringBootDemo1/entity/Department.java
+++ src/main/java/com/himanshu/SpringBootDemo1/entity/Department.java
@@ -1,11 +1,13 @@
 package com.himanshu.SpringBootDemo1.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class Department {
     @Id
+    @Column(name = "DEP_ID")
     private Long departmentId;
     private String departmentName;
     private String departmentAddress;
