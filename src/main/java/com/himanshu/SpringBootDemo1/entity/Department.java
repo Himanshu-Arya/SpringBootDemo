0fa448f86a63b87373e09ccfcd471c5595239b89
Himanshu-Arya <himanshu.arya48@gmail.com>
2022-02-25 23:38:27 +0530

    set table name

--- src/main/java/com/himanshu/SpringBootDemo1/entity/Department.java
+++ src/main/java/com/himanshu/SpringBootDemo1/entity/Department.java
@@ -3,8 +3,10 @@ package com.himanshu.SpringBootDemo1.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "DEP_TABLE")
 public class Department {
     @Id
     @Column(name = "DEP_ID")
